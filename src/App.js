004cfc552125e71ab66181d514953872c0292b87
aporia-coder <zachary_gray12@yahoo.com>
2020-11-14 14:08:50 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.min.css";
+// import dotenv from "dotenv";
+// DOTENV NOT WORKING FOR SOME REASON EVEN WHEN USING CONFIG()
+
+// Google Maps
+import { useLoadScript, GoogleMap } from "@react-google-maps/api";
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat: 52.23484,
+  lng: -0.89732,
+};
 
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API,
+    libraries,
+  });
+  if (loadError) return "Error loading Maps";
+  if (!isLoaded) return "Loading Maps";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left">
+        <h1>Heading</h1>
+        <p>some stuff under the heading</p>
+      </div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      ></GoogleMap>
     </div>
   );
 }
+
+// Remember to try styling map with snazzy maps
 
 export default App;
